c401676860bf17f7155122f38266c26b8c479242
hmdmdbe-stack <hmdmdbe@gmail.com>
2025-12-25 10:34:29 +0300

    Update config.py with hardcoded configuration values
    Replaced environment variables with hardcoded values for BOT_TOKEN, API_ID, API_HASH, OWNER_ID, and DATABASE_URL.

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = "7005270762:AAGs-AJY9iIKiqK6tgO6WuKRhadpPmL_Odc"
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = 8683151
+    API_HASH = "469c492dce3c1f69fc420d1af0e18377"
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -24,9 +24,9 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = 7012982464
     SESSION_NAME = "UPLOADER-X-BOT"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = ""
     MAX_RESULTS = "50"
     PREMIUM_USER = os.environ.get("PREMIUM_USER")
